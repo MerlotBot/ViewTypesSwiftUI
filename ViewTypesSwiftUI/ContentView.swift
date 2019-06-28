0f154f82ae3e549d828fcff76cf617faace62c7b
Nikolaos Antonakakis <n.antonakakis@gmail.com>
2019-06-28 19:28:48 +0200

    create a list of dynamic items

--- ViewTypesSwiftUI/ContentView.swift
+++ ViewTypesSwiftUI/ContentView.swift
@@ -9,22 +9,31 @@
 import SwiftUI
 import Combine
 
-struct RestaurantRow: View {
+struct Restaurant: Identifiable {
+    var id = UUID()
     var name: String
-    
-    var body: some View {
-        Text("Restaurant: \(name)")
-    }
 }
 
+struct RestaurantRow: View {
+    var restaurant: Restaurant
+    
+    var body: some View {
+        Text("Come and eat at \(restaurant.name)")
+    }
+}
 struct ContentView : View {
 
     var body: some View {
-        List {
-            RestaurantRow(name: "Joe's Original")
-            RestaurantRow(name: "The Real Joe's Original")
-            RestaurantRow(name: "Original Joe's")
-        }
+        
+        let first = Restaurant(name: "Joe's Original")
+        let second = Restaurant(name: "The Real Joe's Original")
+        let third = Restaurant(name: "Original Joe's")
+        let restaurants = [first, second, third]
+        
+//        return List(restaurants) { restaurant in
+//            RestaurantRow(restaurant: restaurant)
+//        }
+        return List(restaurants, rowContent: RestaurantRow.init)
     }
 }
 
