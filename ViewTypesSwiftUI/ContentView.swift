8b85a9ba8a96a070969c5b720b19a621dbd02b01
Nikolaos Antonakakis <n.antonakakis@gmail.com>
2019-06-28 15:48:19 +0200

    add a border to a TextField

--- ViewTypesSwiftUI/ContentView.swift
+++ ViewTypesSwiftUI/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView : View {
     var body: some View {
         VStack {
             TextField($name)
+            .textFieldStyle(.roundedBorder)
             Text("Hello, \(name)")
         }
     }
