913dcb33539d121084a1c41b6267690ed2177c8d
Nikolaos Antonakakis <n.antonakakis@gmail.com>
2019-06-28 19:41:48 +0200

    let users move rows in a list

--- ViewTypesSwiftUI/ContentView.swift
+++ ViewTypesSwiftUI/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 import Combine
 
 struct ContentView : View {
-    @State var users = ["Nikos", "Kostas", "Jiannis"]
+    @State var users = ["Nikos", "Kostas", "Jiannis", "Kathrin", "Karin", "Christina"]
     
     var body: some View {
         NavigationView {
@@ -18,14 +18,19 @@ struct ContentView : View {
                 ForEach(users.identified(by: \.self)) { user in
                     Text(user)
                 }
-                .onDelete(perform: delete)
+                .onMove(perform: move)
             }
+                .navigationBarItems(trailing: EditButton())
         }
     }
     
-    func delete(at offsets: IndexSet) {
-        if let first = offsets.first {
-            users.remove(at: first)
+    func move(from source: IndexSet, to destination: Int) {
+        //sort the indexes low to high
+        let reversedSource = source.sorted()
+        
+        // then loop from the back to avoid reordering problems
+        for index in reversedSource.reversed() {
+            users.insert(users.remove(at: index), at: destination)
         }
     }
 }
