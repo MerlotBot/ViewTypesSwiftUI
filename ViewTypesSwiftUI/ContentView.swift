3c45129e16ce43a69d99f0112c9e96b3463b406f
Nikolaos Antonakakis <n.antonakakis@gmail.com>
2019-06-28 14:48:11 +0200

    return views

--- ViewTypesSwiftUI/ContentView.swift
+++ ViewTypesSwiftUI/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello World")
+        Group {
+            if Bool.random() {
+                Image("example-image")
+            } else {
+                Text("Better luck next time")
+            }
+        }
     }
 }
 
