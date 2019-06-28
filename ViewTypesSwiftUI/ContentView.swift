b3bc6afa2c9609fab43927afd7390a368d82bfc3
Nikolaos Antonakakis <n.antonakakis@gmail.com>
2019-06-28 18:24:39 +0200

    read tap and double-tap gestures

--- ViewTypesSwiftUI/ContentView.swift
+++ ViewTypesSwiftUI/ContentView.swift
@@ -10,17 +10,10 @@ import SwiftUI
 
 struct ContentView : View {
     
-    @State private var favoriteColor = 0
-    var colors = ["Red", "Green", "Blue"]
-    
     var body: some View {
-        VStack {
-            SegmentedControl(selection: $favoriteColor) {
-                ForEach(0..<colors.count) { index in
-                    Text(self.colors[index]).tag(index)
-                }
-            }
-            Text("Value: \(colors[favoriteColor])")
+        Text("Tap me!")
+            .tapAction(count: 2) {
+                print("Double Tapped!")
         }
     }
 }
